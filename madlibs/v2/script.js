3ccaf86b7bc84ebf2644879a8b0c1069d158c048
carol <mosnguyen@ucdavis.edu>
2026-02-08 19:05:53 -0800

    jsfix

--- madlibs/v2/script.js
+++ madlibs/v2/script.js
@@ -5,23 +5,28 @@
 
     const allSection = document.querySelectorAll('section');
 
-    document.querySelector('#begin').addEventListender('click', function() {
+    document.querySelector('#begin').addEventListener('click', function() {
         allSection[0].className = "hidden";
         allSection[1].className = "showing choose-2";
     });
 
-    document.querySelector('#pgnext').addEventListender('click', function() {
+    document.querySelector('#next').addEventListener('click', function() {
         allSection[1].className = "hidden";
         allSection[2].className = "showing type-3";
     });
 
-    document.querySelector('#finalpg').addEventListender('click', function() {
+    document.querySelector('#pgnext').addEventListener('click', function() {
         allSection[2].className = "hidden";
         allSection[3].className = "showing type-4";
     });
 
-    document.querySelector('#return').addEventListender('click', function() {
+    document.querySelector('#finalpg').addEventListener('click', function() {
         allSection[3].className = "hidden";
+        allSection[4].className = "showing story-5";
+    });
+
+    document.querySelector('#return').addEventListener('click', function() {
+        allSection[4].className = "hidden";
         allSection[0].className = "showing story-5";
     });
 
